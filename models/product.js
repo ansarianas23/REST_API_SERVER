f2a293655c6acf4e22d2dc91a0a3b5d69fce4039
Ansari Anas <111463267+ansarianas23@users.noreply.github.com>
2024-04-20 18:09:19 +0530

    "added ew validation in schema"

--- models/product.js
+++ models/product.js
@@ -4,12 +4,12 @@ const { Schema } = mongoose;
 
 // Schema
 const productSchema = new Schema({
-    title: {type: String, required:true},
-    description: String,
+    title: {type: String, required:true, unique: true},
+    description: { type: String},
     price: {type: Number, min:[0, 'wrong min price'], required:true},
-    discountPercentage: {type: Number, min:[0, 'wrong min discount'], max:[5, 'wrong max discount']},
+    discountPercentage: {type: Number, min:[0, 'wrong min discount'], max:[50, 'wrong max discount']},
     rating: {type: Number, min:[0, 'wrong min rating'], max:[5, 'wrong max rating']},
-    stock: Number,
+    stock: {type: Number},
     brand: {type:String, required:true},
     category: {type:String, required:true},
     thumbnail: {type:String, required:true},
