c0efb4f208ee29ce335aa6bd7eb6651675ecae78
Ansari Anas <111463267+ansarianas23@users.noreply.github.com>
2024-04-21 14:53:24 +0530

    "few changes"

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const express = require('express');
-const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
+const path = require('path');
+const data = JSON.parse(fs.readFileSync(path.resolve(__dirname,'../data.json'), 'utf-8'));
 const users = data.users;
 
 exports.createUser = (req, res)=>{
